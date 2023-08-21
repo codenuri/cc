615dfd7e3232996a5d2b3dfeed79040d10d27d42
ecourse <smkang@codenuri.co.kr>
2023-08-21 14:29:05 +0900

    Add files via upload

--- DAY1/forward1.cpp
+++ DAY1/forward1.cpp
@@ -21,6 +21,26 @@ void foo(const Point& p) {}
 
 // 상수성 추가없이 lvalue, rvalue 를 모두 받을수 없을까 ?
 // => C++98 은 불가능!!
+// => C++11 부터 가능. rvalue reference 문법 사용
+
+// 방법 4. 2개의 함수 제공
+// 특징 : 상수성 추가 없이 lvalue, rvalue 를 모두 받을수 있다.
+//        결국, 한개의 함수가 아닌 2개의 함수!
+// void foo(Point& p)  {}
+// void foo(Point&& p) {}
+
+// 방법 5. T&&(forwarding reference) 를 사용하면 위 2개 함수를
+//         컴파일러가 자동 생성할수 있다.
+
+// T&& 인자의 의도 : Point&, Point&& 버전의 함수를 자동생성하겠다는 의도
+
+// foo(pt) 로 사용하면, pt는 lvalue 이므로 : T = Point&, T&& = Point& && => Point&
+// foo(Point(1,2))는        rvalue 이므로 : T = Point,  T&& = Point&&
+
+template<typename T>
+void foo(T&& p)
+{
+}
 
 
 int main()
