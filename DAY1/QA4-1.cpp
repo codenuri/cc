93729b6617bb611cd69a817563d6819b1c015c2b
ecourse <smkang@codenuri.co.kr>
2023-08-21 16:21:33 +0900

    Add files via upload

--- DAY1/QA4-1.cpp
+++ DAY1/QA4-1.cpp
@@ -10,7 +10,7 @@
 
 
 #include <iostream>
-
+#include <type_traits>
 class Object
 {
 public:
@@ -21,10 +21,25 @@ public:
 	Object(Object&&) { std::cout << "move" << std::endl; }
 };
 
+// 핵심 : 인자가 T&& 라면, lvalue, rvalue 모두 받을수 있는데, 
+//        각각의 경우, T의 타입을 반드시 외워야 합니다.
+// move(o1)			즉, lvalue 보내면 : T=Object&, T&&= Object& && => Object&
+// move(Object()),  즉, rvalue 보내면 : T=Object,  T&&= Object&&
+
 template<typename T>
-T&& move(T&& obj)
+std::remove_reference_t<T>&& move(T&& obj)
 {
-	return static_cast<T&&>(obj);
+	// 아래 코드는 obj 를 "항상 rvalue" 로 캐스팅하는 코드일까요 ?=> 아닙니다.
+	// move(lvalue 즉, o1) 라면 "static_cast<Object&>(obj)"
+	// move(rvalue )       라면 "static_cast<Object&&>(obj)"
+//	static_cast<T&&>(obj);
+
+	// T&& 가 아닌 Object&& 처럼 명확한 타입을 적은 경우는 rvalue 캐스팅
+//	static_cast<Object&&>(obj);
+
+	// move 는 "항상 rvalue 캐스팅 되어야 합니다."
+	// 방법 : T 가 가진 레퍼런스 타입을 제거한후 && 를 붙이면 됩니다.
+	return static_cast< std::remove_reference_t<T> &&>(obj);
 }
 
 int main()
