13e658fca451e96bf9473e9d8f2bddb8d4459e17
ecourse <smkang@codenuri.co.kr>
2023-08-21 11:17:49 +0900

    Add files via upload

--- DAY1/1_function_parameter_05-2.cpp
+++ DAY1/1_function_parameter_05-2.cpp
@@ -7,14 +7,23 @@ void good(const std::string& s)
 {
 }
 
+
+
 void best(std::string_view s)
 {
 }
+
+
 
 int main()
 {
 	std::string s = "to be or not to be";
 
+	// 아래의 경우는 "const string&" 도 나쁘지 않습니다
 	good(s);
 	best(s);
+
+	// 그런데, 아래 코드를 생각해 보세요
+	good("to be or not to be");
+	best("to be or not to be");
 }
