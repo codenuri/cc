e58c9b341df1334f5e979f4bd14dc9d6fe56cbab
ecourse <smkang@codenuri.co.kr>
2023-08-21 10:02:48 +0900

    Add files via upload

--- DAY1/QA1.cpp
+++ DAY1/QA1.cpp
@@ -1,7 +1,7 @@
 // QA1
 
 // const Rect* 도 임시객체는 받을 수 없나요 ?
-
+// => 안됩니다.
 struct Rect
 {
 	int left, top, right, bottom;
@@ -11,5 +11,7 @@ void f(const Rect* p) {}
 
 int main()
 {
-	f(&Rect());
+	f(&Rect()); // error
+				// 임시객체는 rvalue 입니다.
+				// rvalue 는 주소 연산자를 사용할수 없습니다.
 }
