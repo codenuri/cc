00bde13d1b6211cf46636bad8dfdefa8be56bbe0
ecourse <smkang@codenuri.co.kr>
2023-08-21 16:10:38 +0900

    Add files via upload

--- DAY1/QA4.cpp
+++ DAY1/QA4.cpp
@@ -24,8 +24,20 @@ public:
 };
 
 // C++ 표준의 move를 만들어 봅시다.
+/*
 template<typename T>
 T&& move(T& obj)
+{
+	return static_cast<T&&>(obj);
+}
+*/
+
+// 템플릿 만들때
+// T&  : lvalue 만 인자로 받겠다는 것 
+// T&& : lvalue, rvalue 를 모두 받겠다는 것 
+
+template<typename T>
+T&& move(T&& obj)
 {
 	return static_cast<T&&>(obj);
 }
@@ -38,4 +50,9 @@ int main()
 //	Object o3 = static_cast<Object&&>(o1); // 결국 이코드가 위와 동일한 코드입니다.
 
 	Object o3 = move(o1);
+	Object o4 = move(Object()); // 임시객체는 move가 없어도 rvalue 이므로 이동 생성자 호출
+								// 하지만 move() 안에 rvalue 를 넣을수 없어야 할까요 ?
+								//							   넣을수 있어야 할까요 ?
 }
+// 위 코드는 에러는 없지만, 결과가 잘못됩니다.
+// 왜 그럴까요 ??
