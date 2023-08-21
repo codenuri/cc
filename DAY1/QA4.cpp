8b5d65fef98a43b0dc1ba2dfdbcd34ad0beb387d
ecourse <smkang@codenuri.co.kr>
2023-08-21 16:05:18 +0900

    Add files via upload

--- DAY1/QA4.cpp
+++ DAY1/QA4.cpp
@@ -1,3 +1,14 @@
+// p.set_name("literal")과 p.set_name(std::string("literal"))이 동일한가요 ?
+// => 생성자 배울때 나옵니다. 내일 설명드리겠습니다.
+
+// forward 와 move가 비슷한것 같습니다... ?
+// => 아래 코드로 명확히 이해 하세요..
+// => modern C++ 의 핵심 부분 입니다.
+
+
+// QA4.cpp - git 에서 복사해 오세요
+
+
 #include <iostream>
 
 class Object
@@ -5,13 +16,17 @@ class Object
 public:
 	Object() {}
 
+	// 복사 생성자 : 여기서 자원을 복사하는 코드 작성
 	Object(const Object&) { std::cout << "copy" << std::endl; }
+
+	// move 생성자 : 여기서 자원을 이동하는 코드 작성
 	Object(Object&&)      { std::cout << "move" << std::endl; }
 };
 
 int main()
 {
 	Object o1;
-	Object o2 = o1;
-	Object o3 = std::move(o1);
+	Object o2 = o1;				// 복사 생성자 호출
+//	Object o3 = std::move(o1);	// 이동 생성자 호출
+	Object o3 = static_cast<Object&&>(o1); // 결국 이코드가 위와 동일한 코드입니다.
 }
