8dc9644302fb557369ad83a73a5c7709a045bf3d
ecourse <smkang@codenuri.co.kr>
2023-08-22 15:10:13 +0900

    Add files via upload

--- DAY2/3_explicit1-1.cpp
+++ DAY2/3_explicit1-1.cpp
@@ -2,12 +2,17 @@
 #include <string>
 #include <vector>
 
-void f1(std::string s) {}
+void f1(std::string s)      {}
 void f2(std::vector<int> v) {}
 
 int main()
 {
 	// 아래 2줄의 코드는 되는게 좋은지 안되는 것이 좋은지 생각해 보세요
-	f1("hello");
-	f2(10);
+	f1("hello"); // 되는게 좋습니다. ok. 
+	f2(10);		 // 안되는게 좋습니다. error
+
+	// 의미
+	// string 의 생성자는 explicit 가 아닙니다.
+	std::string s1("hello");  // ok
+	std::string s2 = "hello"; // ok
 }
