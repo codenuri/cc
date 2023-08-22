ac25c06e927ccbbf2c122cbd5d33e59d4e7e21be
ecourse <smkang@codenuri.co.kr>
2023-08-22 09:20:47 +0900

    Add files via upload

--- DAY2/QA1.cpp
+++ DAY2/QA1.cpp
@@ -2,11 +2,17 @@
 #define MAX1 10
 constexpr int MAX2 = 10;
 
+#define SQUARE(n)  n * n
+
 int main()
 {
 	int n1 = MAX1;
 	int n2 = MAX2;
+
+	int s = SQUARE(++n1); // 
 }
 // 매크로 : 컴파일을 하기 전에 전처리기가 소스를 변경하는것
 // g++ QA1.cpp -E -P
 // cl  QA1.cpp /EP
+
+// 시작 버튼
