38707df4d5d11ef1bbf762c4910022a53470d650
ecourse <smkang@codenuri.co.kr>
2023-08-22 15:39:06 +0900

    Add files via upload

--- DAY2/3_explicit3-1.cpp
+++ DAY2/3_explicit3-1.cpp
@@ -7,14 +7,16 @@ public:
 
 	Point(int a) : x(a), y(0) {}
 
-
-	operator int() const { return x; }
+	// C++11 부터, 생성자뿐 아니라 변환연산자에도 explicit 사용가능합니다
+	// "암시적 변환은 허용하지 않고, 명시적 변환만 허용"
+	explicit operator int() const { return x; }
 };
 
 int main()
 {
 	Point pt(1, 2);
 
-	int n1 = pt;
+//	int n1 = pt;                   // error. explicit 이므로!
+	int n2 = static_cast<int>(pt); // ok
 
 }
