e338f57648efd9687709713e52f57033d6987bbc
ecourse <smkang@codenuri.co.kr>
2023-08-22 14:19:47 +0900

    Add files via upload

--- DAY2/2_constructor_06.cpp
+++ DAY2/2_constructor_06.cpp
@@ -1,6 +1,9 @@
 // 생성자와 가상함수
 #include <iostream>
-
+// 핵심 : 생성자에서는 가상함수가 동작하지 않습니다.
+//        Base() 생성자 에서는 항상 Base::init 호출합니다.
+//        => 이유는 아래 코드에서 Base::init 이 아닌 Derived::init 이 호출되면
+//			 초기화 되지 않은 x를 사용하게 됩니다.
 struct Base
 {
 	Base() { init(); } // 어느 함수 호출할지 예측해 보세요
