15fd19da1c427db8c9dee1fe0466f2df33756dc6
ecourse <smkang@codenuri.co.kr>
2023-08-22 14:49:04 +0900

    Add files via upload

--- DAY2/3_explicit1.cpp
+++ DAY2/3_explicit1.cpp
@@ -3,10 +3,16 @@
 class Vector
 {
 public:
-	Vector(int sz) {}
+//	Vector(int sz) {}
+
+	// explicit 생성자 : 직접 초기화만 사용할수 있다.
+	// 1. 복사 초기화(copy initialization) 을 할수 없고
+	// 2. 생성자에 의한 변환도 할수 없다!!
+
+	explicit 	Vector(int sz) {}
 };
 
-void fn(Vector v) {}
+void fn(Vector v) {} // Vector v = 10
 
 int main()
 {
@@ -24,9 +30,11 @@ int main()
 	v1 = 10; // 초기화가 아닌 대입 코드 입니다.
 			 // "10" 을 가지고 Vector 의 임시객체 생성후, v1에 대입!
 			 // v1.operator=(Vector(10)) 의 의미!
-			// 마치 10 => Vector 변환처럼 보이게 됩니다.
-			// 그래서 인자가 한개인 생성자를 "변환 생성자" 라고도 합니다.
+			 // 마치 10 => Vector 변환처럼 보이게 됩니다.
+			 // 그래서 인자가 한개인 생성자를 "변환 생성자" 라고도 합니다.
 
 	// 핵심 3. 복사 초기화나 생성자에 의한 변환은 항상 버그의 위험이 있습니다
 	fn(10); // 왜?? 에러가 없을지 생각해 보세요
+			// 함수의 인자 전달은 "복사 초기화" 과정입니다
+			// Vector v = 10
 }
