e85175161f82b85d00341ae718127584b567835a
ecourse <smkang@codenuri.co.kr>
2023-08-22 13:18:13 +0900

    Add files via upload

--- DAY2/2_constructor_01.cpp
+++ DAY2/2_constructor_01.cpp
@@ -5,6 +5,9 @@ class People1
 	std::string name;
 	int age;
 public:
+	// 아래 코드는
+	// 1. string name(s) 로 초기화 된것. 
+	// => 즉, 1회의 함수 호출
 	People1(const std::string& s, int a) : name(s), age(a)  // good
 	{	
 	}
@@ -14,14 +17,21 @@ class People2
 {
 	std::string name;
 	int age;
-public:
-	People2(const std::string& s, int a)	
+public:	
+	// 아래 코드는 
+	// 1. string name 에 대해서 디폴트 생성자 호출후
+	// 2. name.operator=(s) 로 값을 넣은것
+	// 즉, 2회의 함수 호출
+										 // 컴파일러가 추가한 코드
+	People2(const std::string& s, int a) //	: name()  <= name의 디폴트 생성자호출
 	{										
 		name = s;	// bad  
 		age = a;
 	}
 };
 
+// People2 p2(이름,2); // 이순간의 생성자 호출을 생각해 봅시다.
+	
 
 
 int main()
@@ -34,7 +44,7 @@ int main()
 	// 초기화 vs 대입
 	// primitive type 인 경우는 대부분 유사.
 	// user define type 의 경우는 초기화가 빠릅니다.
-
+	/*
 	int a = 0;	// 초기화, 선언과 동시에 값 넣기
 
 	int b;
@@ -47,4 +57,5 @@ int main()
 	std::string s2;	// 1. string 디폴트 생성자 호출
 	s2 = "hello";	// 2. s2.operator=("hello"). 즉, 대입 연산자 호출
 					// => 2회의 함수 호출로 값을 넣은 것
+	*/
 }
