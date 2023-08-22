a5aea7811ff852ced19fa3f8ad60e37a6bc5559b
ecourse <smkang@codenuri.co.kr>
2023-08-22 11:29:17 +0900

    Add files via upload

--- DAY2/1_const_member_function_06-1.cpp
+++ DAY2/1_const_member_function_06-1.cpp
@@ -7,8 +7,13 @@ class Test
 {
 public:
 	// C++98 ~ C++20 스타일
-	void f1()       {}
-	void f1() const {}
+	void f1()       {} // void f1(Test* this) 
+	void f1() const {} // void f1(const Test* this) 
+
+	// C++23 새로운 문법으로 위 코드처럼 2개 만들기!
+	// => 핵심 : const 키워드가 함수 () 뒤가 아닌, 함수 인자로 놓이게 됩니다.
+	void f2(this Test&       self) {}
+	void f2(this const Test& self) {}
 };
 
 int main()
