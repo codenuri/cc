f5a25c3e6216d5de3f0181d0177faf0a711b126c
ecourse <smkang@codenuri.co.kr>
2023-08-23 14:06:08 +0900

    Add files via upload

--- DAY3/7_strength2.cpp
+++ DAY3/7_strength2.cpp
@@ -12,7 +12,15 @@ int main()
 	std::string s1 = "AAA";
 	std::string s2 = "BBB";
 
-	auto ret = max1(s1, s2);
+	auto ret1 = max1(s1, s2); // ret1 결국 s2의 복사본 입니다.
+
+	const std::string& ret2 = max1(s1, s2);
+							  // ret2 는 s2의 별명 입니다.
+	//=====================================
+	// 이 예제의 핵심 - 2개의 인자가 임시객체(rvalue) 일때
+	// 아래 처럼 받는 ret3은 안전할까요 ?
+	const std::string& ret3 = max1(std::string("AAA"),
+								   std::string("BBB"));
 }
 
 
