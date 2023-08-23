d4da508b7266a8bd6ebd7325915034801c560276
ecourse <smkang@codenuri.co.kr>
2023-08-23 09:37:09 +0900

    Add files via upload

--- DAY3/1_new3.cpp
+++ DAY3/1_new3.cpp
@@ -18,13 +18,28 @@ public:
 	vector(int sz, const T& value) : size(sz)
 	{
 //		buff = new T;	// 이렇게 하면 T 는 반드시 디폴트 생성자가 있어야 합니다
-	
 		buff = static_cast<T*>(operator new(sizeof(T) * sz));
 
-		for (int i = 0; i < sz; ++i)
+		int cnt = 0;
+		try
 		{
-//			new( &buff[i] ) T;      // 디폴트 생성자!!
-			new(&buff[i]) T(value); // 복사 생성자 사용
+			for (int i = 0; i < sz; ++i)
+			{
+	//			new( &buff[i] ) T;      // 디폴트 생성자!!
+				new(&buff[i]) T(value); // 복사 생성자 사용
+				++cnt;
+			}
+		}
+		catch (...)
+		{
+			// 복사생성자 호출에 성공했던 객체는 소멸자를 호출해야 한다.
+			for (int i = 0; i < cnt; i++)
+				buff[i].~T();
+
+			operator delete(buff);
+			size = 0;
+
+			throw; // 다시 예외 전달!
 		}
 	}
 	~vector()
