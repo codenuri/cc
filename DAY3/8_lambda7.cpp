06fc66f127f7a3902017996de62bdc8f6efc9654
ecourse <smkang@codenuri.co.kr>
2023-08-23 16:12:03 +0900

    Add files via upload

--- DAY3/8_lambda7.cpp
+++ DAY3/8_lambda7.cpp
@@ -8,9 +8,17 @@ public:
 	// 있습니다.
 	using F = int(*)(int, int);
 
-	operator F() { return 함수 주소; }
-};
+//	operator F() { return &CompilerGeneratedName::operator(); }
+					// => error
+					// => 멤버 함수는 "this"가 추가되는 함수 입니다.
+					// => 따라서 일반 함수 포인터에 멤버 함수 주소 담을수 없습니다
 
+	// 함수 포인터로의 변환을 위해서 "operator()" 와 동일한 일을 하는 
+	// static 멤버 함수가 필요
+	static auto helper(int a, int b) { return a + b; }
+
+	operator F() { return &CompilerGeneratedName::helper; }
+};
 
 
 int main()
