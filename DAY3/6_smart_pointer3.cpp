b4c027530aa56fff8fb5e755811394445cca089c
ecourse <smkang@codenuri.co.kr>
2023-08-23 13:38:55 +0900

    Add files via upload

---
+++ DAY3/6_smart_pointer3.cpp
@@ -0,0 +1,24 @@
+﻿// R.11: Avoid calling new and delete explicitly
+// R.1: Manage resources automatically using resource handles and RAII (Resource Acquisition Is Initialization)
+// E.6: Use RAII to prevent leaks
+
+#include <iostream>
+#include <memory>
+
+int main()
+{
+	// 자원을 아래처럼 할당했는데..
+	int* p = new int;				
+
+	std::shared_ptr<int> sp1(p);									
+	std::shared_ptr<int> sp2 = sp1;  
+	std::shared_ptr<int> sp3(p);  
+
+	// RAII : Resource Acquision Is Initialization
+	int* p = new int;				
+	std::shared_ptr<int> sp1(p);	
+
+	std::shared_ptr<int> sp1(new int);	
+
+	std::shared_ptr<int> sp1 = std::make_shared<int>(0); 
+}
