ccd04927765e486550597ebd6fa2a76bae3accee
ecourse <smkang@codenuri.co.kr>
2023-08-23 14:14:15 +0900

    Add files via upload

---
+++ DAY3/7_Strength3.cpp
@@ -0,0 +1,28 @@
+#include <string>
+#include <iostream>
+
+template<typename T>
+const T& max1(const T& lhs, const T& rhs)
+{
+	return lhs < rhs ? rhs : lhs;
+}
+
+// STL 로 부터 상속받는 것은 좋지 않습니다. 실전에서는 사용하지 마세요
+// 단지, 테스트용
+class String : public std::string
+{
+public:
+	using std::string::basic_string; // 생성자 상속
+
+	~String() { std::cout << "~String" << std::endl; }
+};
+
+int main()
+{
+	const String& ret3 = max1(String("AAA"),String("BBB"));
+
+	std::cout << "-------------" << std::endl;
+}
+
+
+
