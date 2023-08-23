75bee78b5f8a26acfdf10a1c0d97ec93ec0f38f2
ecourse <smkang@codenuri.co.kr>
2023-08-23 11:42:22 +0900

    Add files via upload

--- DAY3/4_convenient_function_template1-1.cpp
+++ DAY3/4_convenient_function_template1-1.cpp
@@ -23,10 +23,15 @@ int main()
 
 //	PAIR p2(1, 3.4);
 
-	auto p2 = make_pair(1, 3.4);
+	// make_pair 도 템플릿 이므로 정확한 표기법은 아래 코드 입니다.
+	auto p2 = make_pair<int, double>(1, 3.4); 
+
+	// 하지만 make_pair은 함수 템플릿 이므로 "C++17 이전에도 타입 생략" 가능합니다.
+	auto p3 = make_pair(1, 3.4);
 }
 
-
+// make_pair 같은 기술을 "Object Generator" 라고 하는데,
+// cppreference.com 에서는 "convenient function template" 이라는 용어를 사용합니다.
 
 
 
