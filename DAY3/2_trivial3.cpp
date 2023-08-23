e0196d27f9193682f0e06c16a0c63bf693e31119
ecourse <smkang@codenuri.co.kr>
2023-08-23 10:17:04 +0900

    Add files via upload

---
+++ DAY3/2_trivial3.cpp
@@ -0,0 +1,57 @@
+#include <iostream>
+
+class Point
+{
+	int x;
+	int y;
+public:
+	Point(int a, int b) : x(a), y(b) {}
+	~Point() {}
+};
+
+template<typename T> class vector
+{
+	T* buff;
+	int size;
+public:
+	vector(int sz, const T& value) : size(sz)
+	{
+		buff = static_cast<T*>(operator new(sizeof(T) * sz));
+
+		int cnt = 0;
+		try
+		{
+			for (int i = 0; i < sz; ++i)
+			{
+				new(&buff[i]) T(value); 
+				++cnt;
+			}
+		}
+		catch (...)
+		{
+			for (int i = 0; i < cnt; i++)
+				buff[i].~T();
+
+			operator delete(buff);
+			size = 0;
+			buff = nullptr;
+
+			throw; 
+		}
+	}
+	~vector()
+	{
+		for (int i = 0; i < size; i++)
+			buff[i].~T();
+
+		operator delete(buff);
+		size = 0;
+	}
+};
+
+int main()
+{
+	Point pt(0, 0);
+	vector<Point> v(10, pt);
+
+}
