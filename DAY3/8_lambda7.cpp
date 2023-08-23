b231d0227524d45022ea98f7e65f7a8eefc69d0e
ecourse <smkang@codenuri.co.kr>
2023-08-23 16:08:12 +0900

    Add files via upload

--- DAY3/8_lambda7.cpp
+++ DAY3/8_lambda7.cpp
@@ -2,8 +2,13 @@
 class CompilerGeneratedName
 {
 public:
+	auto operator()(int a, int b) const { return a + b; }
 
+	// 함수 포인터로의 변환을 위해서 컴파일러가 만드는 클래스에는 아래 멤버가 
+	// 있습니다.
+	using F = int(*)(int, int);
 
+	operator F() { return 함수 주소; }
 };
 
 
@@ -11,8 +16,7 @@ public:
 int main()
 {
 	int(*f1)(int, int) = CompilerGeneratedName();
-
-
+						// 임시객체.operator 함수포인터타입()
 
 
 
